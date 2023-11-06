e6f87c78834c86718c932d66a47f386f4938d89e
Machine <devtools189@gmail.com>
2023-11-06 18:19:13 -0300

    Criado tabela parao banco de dados

--- api/src/main/java/br/com/projeto/api/controle/Controle.java
+++ api/src/main/java/br/com/projeto/api/controle/Controle.java
@@ -1,5 +1,16 @@
 package br.com.projeto.api.controle;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Controle {
+
+    // Método de retorno para testar 
+    @GetMapping("/")
+    public String teste(){
+        return "Hello World!";
+    }
+
     
 }
--- api/src/main/java/br/com/projeto/api/modelo/Cliente.java
+++ api/src/main/java/br/com/projeto/api/modelo/Cliente.java
@@ -1,5 +1,30 @@
 package br.com.projeto.api.modelo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+/*criar a tabela do banco de dados; 
+  Trafega informações atraves da API */
+
+@Entity
+@Table(name = "clientes")  
+@Getter
+@Setter
 public class Cliente {
+
+    @Id    // id do tipo auto_increment
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long codigo;
+
+    private String nome;
+
+    private int idade;
+
+    private String cidade;
     
 }
