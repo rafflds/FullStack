1c1b823b03c3c64776f57aca8bf00e64f03c0497
Machine <devtools189@gmail.com>
2023-11-06 18:49:40 -0300

    Criado rota de cadastramento

--- api/src/main/java/br/com/projeto/api/controle/Controle.java
+++ api/src/main/java/br/com/projeto/api/controle/Controle.java
@@ -1,16 +1,48 @@
 package br.com.projeto.api.controle;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.projeto.api.modelo.Cliente;
+import br.com.projeto.api.repositorio.Repositorio;
 
 @RestController
 public class Controle {
 
+    /*informam ao Spring para procurar uma instância adequada de uma classe de dependência 
+    e injetá-la automaticamente no componente que requer essa dependência. */
+    @Autowired  
+    private Repositorio acao;
+
+    
+    /*
+    Rota de cadastramento de clientes
+    * Thunder Client
+    * (Post) localhost:8080
+    {
+        "nome":"Thales",
+        "idade": 5,
+        "cidade":"Candangolândia"
+    }
+    */ 
+    @PostMapping("/")
+    public Cliente cadastrar(@RequestBody Cliente c){
+        return acao.save(c);
+    }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
     // Método de retorno para testar 
-    @GetMapping("/")
+    /*@GetMapping("/")
     public String teste(){
         return "Hello World!";
     }
-
+    */
     
 }
--- api/src/main/java/br/com/projeto/api/repositorio/repositorio.java
+++ api/src/main/java/br/com/projeto/api/repositorio/repositorio.java
@@ -1,5 +1,12 @@
 package br.com.projeto.api.repositorio;
 
-public interface repositorio {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.projeto.api.modelo.Cliente;
+
+// Ações SQL (alterar, excluir, cadastrar...)
+@Repository                     // Cliente = class criou a tabela
+public interface Repositorio extends CrudRepository<Cliente, Long> {
     
 }
