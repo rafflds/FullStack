33183a3756f797403697cd11e558b7c61c43d20f
Machine <devtools189@gmail.com>
2023-11-06 22:45:59 -0300

    app.module, cliente.ts, CSS3, Html e ts alterados

--- api-front/src/app/app.module.ts
+++ api-front/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { PrincipalComponent } from './principal/principal.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,9 +12,21 @@ import { PrincipalComponent } from './principal/principal.component';
     PrincipalComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+/*
+FormsModule:
+  - Responsável por manipular elementos de formulário. Ex:input, select, textarea
+  - permite que envie, extraia dados
+
+HttpClientModule:
+  - Responsável por requisições: GET, POST, PUT e DELETE 
+*/ 
---
+++ api-front/src/app/modelo/Cliente.ts
@@ -0,0 +1,10 @@
+/*
+Utilizado para manipular as informações, indo e vindo da API
+*/
+export class Cliente{
+
+    codigo:number = 0;
+    nome:string = '';
+    idade:number = 0;
+    cidade:string = '';
+}
