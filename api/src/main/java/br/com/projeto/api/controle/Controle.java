5ef45824d72937d33f8b69c1ec2c33c668286de8
Machine <devtools189@gmail.com>
2023-11-06 21:50:05 -0300

    Criado o projeto front-end Angular

--- api/src/main/java/br/com/projeto/api/controle/Controle.java
+++ api/src/main/java/br/com/projeto/api/controle/Controle.java
@@ -1,6 +1,7 @@
 package br.com.projeto.api.controle;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,7 @@ import br.com.projeto.api.modelo.Cliente;
 import br.com.projeto.api.repositorio.Repositorio;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class Controle {
 
     /*informam ao Spring para procurar uma instância adequada de uma classe de dependência 
