80043478a0be0fae5631dcdd9870c105819af178
Machine <devtools189@gmail.com>
2023-11-07 21:04:36 -0300

    atualizado o front

--- api-front/src/app/principal/principal.component.ts
+++ api-front/src/app/principal/principal.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Cliente } from '../modelo/Cliente';
+import { ClienteService } from '../servico/cliente.service';
 
 @Component({
   selector: 'app-principal',
@@ -7,7 +9,29 @@ import { Component } from '@angular/core';
 })
 export class PrincipalComponent {
 
+  // Objeto do tipo cliente
+  cliente = new Cliente();
+
   // Variável para visibilidade dos botões
   btnCadastro:boolean = true;
+
+  // Json de clientes
+  clientes:Cliente[] = [];
+
+  // LIberando acesso ao cliente.service.ts
+  constructor(private servico:ClienteService){}
+
+  // Método de seleção de clientes(sem retorno)
+  selecionar():void{
+    // Acessando o método do cliente.service.ts (seleção de * clientes)
+    this.servico.selecionar()
+    .subscribe(retorno => this.clientes = retorno);  // retorno = lista de clientes
+  }
+
+  // Método de inicialização (Nativo do Angular)
+  ngOnInit(){
+    this.selecionar();
+  }
+
 
 }
--- api-front/src/app/servico/cliente.service.ts
+++ api-front/src/app/servico/cliente.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Cliente } from '../modelo/Cliente';
 
 @Injectable({
   providedIn: 'root'
@@ -12,4 +14,18 @@ export class ClienteService {
   /*Construtor (realizar requisições da API)
   Necessita dele para cadastrar, selecionar, alterar e remover */ 
   constructor(private http:HttpClient) { }
+
+  // Método para selecionar todos os clientes
+  selecionar():Observable<Cliente[]>{
+    return this.http.get<Cliente[]>(this.url);
+  }
+
+  /*
+  Método para cadastrar clientes 
+  - obter os dados do banco cliente
+  - Observable: vai retorna o cliente com estas informações
+  */ 
+  cadastrar(obj:Cliente):Observable<Cliente>{
+    return this.http.post<Cliente>(this.url, obj);
+  }
 }
